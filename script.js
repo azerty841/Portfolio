f730cf504f3c346785ce6c5ece517923cfe61efe
azerty841 <164852097+azerty841@users.noreply.github.com>
2025-11-18 20:16:58 +0100

    Portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ouvre le menu latéral
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px"; // Largeur du menu
+}
+
+// Ferme le menu latéral
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  
+}
